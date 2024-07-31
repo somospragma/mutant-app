67d29af8e283b9917381aec51202a67ef69bfb65
Cristian Alfredo Muñoz Alvarez <cristian.munoza@pragma.com.co>
2024-07-31 02:13:09 -0500

    changes for amazon q workshop

--- src/main/java/com/pragma/mutant/mutantapp/config/DynamoConfig.java
+++ src/main/java/com/pragma/mutant/mutantapp/config/DynamoConfig.java
@@ -1,5 +1,6 @@
 package com.pragma.mutant.mutantapp.config;
 
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
@@ -13,7 +14,9 @@ public class DynamoConfig {
 
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
-        return AmazonDynamoDBClientBuilder.defaultClient();
+        return AmazonDynamoDBClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1)
+                .build();
     }
 
     @Bean
